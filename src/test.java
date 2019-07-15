55cf227f250292e64e52cee362b3498ee389c4f5
Bravehyy <1147655179@qq.com>
2019-07-15 17:10:46 +0800

    测试

---
+++ src/test.java
@@ -0,0 +1,17 @@
+/**
+ * @ClassName: test
+ * @Description: TODO
+ * @Author: Administrator
+ * @Date: 2019/7/15 0015 上午 10:35
+ * @Version: 1.0
+ */
+public class test {
+
+
+    public static void main(String[] args) {
+        String s="sdfg";
+        System.out.println(s);
+    }
+
+
+}
